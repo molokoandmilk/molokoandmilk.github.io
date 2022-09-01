6d35f1282a0a9e788d871ce41beb7f6d202c0f9b
molokoandmilk <106869516+molokoandmilk@users.noreply.github.com>
2022-09-02 00:18:57 +0300

    project

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+const dialog = document.querySelector('.dialog-container');
+
+function openDialog() {
+    dialog.classList.add('shown')
+}
+
+function closeDialog() {
+    dialog.classList.remove('shown')
+}
