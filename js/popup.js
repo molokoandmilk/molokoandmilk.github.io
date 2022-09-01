b9725e3364caa35543c47321a79e6ddab01bb5d0
molokoandmilk <106869516+molokoandmilk@users.noreply.github.com>
2022-09-02 00:35:47 +0300

    Delete js directory

--- js/popup.js
+++
@@ -1,9 +0,0 @@
-const dialog = document.querySelector('.dialog-container');
-
-function openDialog() {
-    dialog.classList.add('shown')
-}
-
-function closeDialog() {
-    dialog.classList.remove('shown')
-}
